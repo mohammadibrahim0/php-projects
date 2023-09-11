3f4ad2213d53a55a841c28bb400d15336dccd057
mohammadibrahim0 <84647707+mohammadibrahim0@users.noreply.github.com>
2023-09-11 15:14:02 +0300

    Add files via upload

---
+++ github/lab2.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title> lab2</title>
+</head>
+<body>
+<h1>Mohammed ibrahim-202011048 </h1>
+
+<?php
+ function add($x, $y) {
+        return $x + $y;
+    }
+
+  function subtract($x, $y) {
+        return $x - $y;
+    }
+
+function divide($x, $y){
+return $x/$y;
+}
+
+function multiply($x, $y){
+return $x*$y;
+}
+$a=5;
+$b=5;
+ echo "Add: "       . add($a, $b) . "<br>";
+ echo "Sub: "    . subtract($a, $b) . "<br>";
+ echo "Mul: " . multiply($a, $b) . "<br>";
+ echo "Div: "       . divide($a, $b) . "<br>";
+
+?>
+</body>
+</html
