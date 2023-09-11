702529c84edc74aabad1eaad17f4d01a9bac1071
mohammadibrahim0 <84647707+mohammadibrahim0@users.noreply.github.com>
2023-09-11 13:39:29 +0300

    Add files via upload

---
+++ github/n1.php
@@ -0,0 +1,33 @@
+<?php
+$n = 1;
+$s = "1";
+
+$n1 =$n.$s; // 
+$n2 = $n1 . "10 little penguins";
+$n3 = "hello";
+$n4 = " world";
+$n5 = $n3.$n4;
+$n6 = $n3.$n4; 
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title> lab1 </title>
+</head>
+<body>
+<h1>Mohammed ibrahim-202011048 </h1>
+
+<?php
+ echo $n . "<br>";
+ echo $s . "<br>";
+echo $n1 . "<br>";
+echo $n2 . "<br>";
+echo $n3 . "<br>";
+echo $n4 . "<br>";
+echo $n5 . "<br>";
+echo $n6 . "<br>";
+?>
+</body>
+</html
